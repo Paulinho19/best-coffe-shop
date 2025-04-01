4e5b25854b6732840fdccb16af57f955f3705afe
Paulo <paulohsantosrocha@gmail.com>
2025-04-01 13:35:05 -0300

    feat: add wifi button

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-orange-300`}
       >
         <Header />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,41 @@
+"use client";
+
 import Products from "@/components/Products";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [password, setPassword] = useState("********");
+
+  const handlePasswordChange = () => {
+    setPassword("Tia@rosa!");
+  };
+
   return (
-    <main className="flex min-h-[612px] flex-col items-center p-4">
+    <main className="flex min-h-[612px] flex-col items-center p-4 ">
       <h1 className="text-4xl font-bold text-orange-800">
         Tia Rosa, Coffe Shop
       </h1>
-      <p className="mt-4 text-2xl text-gray-700">
+      <p className="mt-4 text-3xl text-gray-800">
         Bem-vindo ao nosso café! Aqui você encontra os melhores cafés da cidade.
       </p>
 
       <Products />
+
+      <div className="flex items-center justify-center gap-2 ml-58">
+        <Button
+          className="mt-4 h-[41px] cursor-pointer"
+          onClick={handlePasswordChange}
+        >
+          Ver senha do Wi-fi
+        </Button>
+        <input
+          type="text"
+          defaultValue={password}
+          value={password}
+          className="mt-4 p-2  rounded font-bold text-[18px]"
+        />
+      </div>
     </main>
   );
 }
